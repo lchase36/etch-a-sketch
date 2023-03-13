2c896ff3591269229f2f201aae41571cc25bea66
lchase36 <chaselesh36@gmail.com>
2023-03-12 22:45:25 -0700

    Added user resizing and shading of cells on each mouseover event

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,14 +1,22 @@
 const container = document.getElementById('container');
-let gridSize = 16;
+const button = document.getElementById('reset');
+button.addEventListener('click', () => {
+	rebuildGrid(prompt("Enter grid size (<=100):"));
+});
+rebuildGrid(16);
 
-container.style.gridTemplateColumns = `repeat(${gridSize}, ${100/gridSize}%)`;
-container.style.gridTemplateRows = `repeat(${gridSize}, ${100/gridSize}%)`;
-for (let i = 0; i < gridSize; i++) {
-	for (let j = 0; j < gridSize; j++) {
-		const div = document.createElement('div');
-		div.classList.add('square');
-		div.addEventListener('mouseover', assignRandomColor);
-		container.appendChild(div);
+function rebuildGrid (size) {
+	let gridSize = Math.min(parseInt(size), 100);
+	container.replaceChildren();
+	container.style.gridTemplateColumns = `repeat(${gridSize}, ${100/gridSize}%)`;
+	container.style.gridTemplateRows = `repeat(${gridSize}, ${100/gridSize}%)`;
+	for (let i = 0; i < gridSize; i++) {
+		for (let j = 0; j < gridSize; j++) {
+			const div = document.createElement('div');
+			div.style.filter = 'brightness(100%)';
+			div.addEventListener('mouseover', assignRandomColor);
+			container.appendChild(div);
+		}
 	}
 }
 
@@ -20,7 +28,12 @@ function assignRandomColor () {
 	let r = randomNumber(255);
 	let g = randomNumber(255);
 	let b = randomNumber(255);
+	if (this.style.backgroundColor != '') {
+		let filter = this.style.filter;
+		let filterNum = parseInt(filter.replace(/[^0-9]/g, ''));
+		let filterNew = Math.max(filterNum-10, 0);
+		this.style.filter = `brightness(${filterNew}%)`;
+	}
 	this.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
 }
 
-const divs = document.getElementsByClassName('square');
